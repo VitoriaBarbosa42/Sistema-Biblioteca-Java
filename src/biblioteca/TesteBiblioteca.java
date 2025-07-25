3287bab6b4bb3df2cc8048ccbff479826b8f2475
VitoriaBarbosa42 <vitoria42.barbosa@gmail.com>
2025-07-25 20:03:22 -0300

    ...

--- src/biblioteca/TesteBiblioteca.java
+++ src/biblioteca/TesteBiblioteca.java
@@ -1,14 +1,12 @@
 package biblioteca;
 
-import livro.Livro;
-
 public class TesteBiblioteca {
 
 	public static void main(String[] args) {
 		
-		Livro livro1 = new Livro("abc", "def", "gh1", 2023, 4, 200);
-		
-		EstoquedeLivros b1 = new EstoquedeLivros();
+//		Livro livro1 = new Livro("abc", "def", "gh1", 2023, 4, 200);
+//		
+//		EstoquedeLivros b1 = new EstoquedeLivros();
 		
 	}
 
